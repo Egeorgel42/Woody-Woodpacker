9050b0aab6646bd68ee792d35f9f73f2ae1c0559
Ethan Georgel <egeorgel@c4r2p5.42nice.fr>
2025-11-26 17:54:28 +0100

    added PIE executable check

--- src/elf32_parsing.c
+++ src/elf32_parsing.c
@@ -1,5 +1,26 @@
 #include "woody.h"
 
+static void	check_PIE32(int fd, char **err_msg, Elf32_Ehdr *header)
+{
+	bool	is_executable = false;
+
+	Elf32_Phdr	*pgr = malloc(header->e_phentsize * header->e_phnum);
+	lseek(fd, header->e_phoff, SEEK_SET);
+	for (int i = 0; i < header->e_phnum; i++)
+	{
+		size_t rd = read(fd, &pgr[i], header->e_phentsize);
+		if (rd < header->e_phentsize)
+			vprintf_exit(err_msg[ERR_READ], strerror(errno));
+	}
+	for (int i = 0; i < header->e_phnum; i++)
+	{
+		if (pgr[i].p_type == PT_INTERP)
+			is_executable = true;
+	}
+	if (!is_executable)
+		vprintf_exit(err_msg[ERR_NEXEC]);
+	free(pgr);
+}
 void	parse_elf32(int fd, char **err_msg)
 {
 	Elf32_Ehdr	header;
@@ -10,4 +31,6 @@ void	parse_elf32(int fd, char **err_msg)
 		vprintf_exit(err_msg[ERR_NEXEC]);
 	if (header.e_version == EV_NONE || header.e_version != EV_CURRENT)
 		vprintf_exit(err_msg[ERR_ELFHDR]);
+	if (header.e_type == ET_DYN)
+		check_PIE32(fd, err_msg, &header);
 }
--- src/elf64_parsing.c
+++ src/elf64_parsing.c
@@ -1,15 +1,26 @@
 #include "woody.h"
-/*
+
 static void	check_PIE64(int fd, char **err_msg, Elf64_Ehdr *header)
 {
+	bool	is_executable = false;
+
+	Elf64_Phdr	*pgr = malloc(header->e_phentsize * header->e_phnum);
+	lseek(fd, header->e_phoff, SEEK_SET);
 	for (int i = 0; i < header->e_phnum; i++)
 	{
-		Elf64_Phdr	pgr;
-		size_t rd = read(fd, &pgr, sizeof(Elf64_Ehdr));
-
+		size_t rd = read(fd, &pgr[i], header->e_phentsize);
+		if (rd < header->e_phentsize)
+			vprintf_exit(err_msg[ERR_READ], strerror(errno));
 	}
-
-}*/
+	for (int i = 0; i < header->e_phnum; i++)
+	{
+		if (pgr[i].p_type == PT_INTERP)
+			is_executable = true;
+	}
+	if (!is_executable)
+		vprintf_exit(err_msg[ERR_NEXEC]);
+	free(pgr);
+}
 
 void	parse_elf64(int fd, char **err_msg)
 {
@@ -21,6 +32,6 @@ void	parse_elf64(int fd, char **err_msg)
 		vprintf_exit(err_msg[ERR_NEXEC]);
 	if (header.e_version == EV_NONE || header.e_version != EV_CURRENT)
 		vprintf_exit(err_msg[ERR_ELFHDR]);
-	//if (header.e_type == ET_DYN)
-		//check_PIE64(fd, err_msg, &header);
+	if (header.e_type == ET_DYN)
+		check_PIE64(fd, err_msg, &header);
 }
